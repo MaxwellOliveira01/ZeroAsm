fa6233ee7675976e36ba5f3dbca38ac72f443d85
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2024-12-23 18:09:41 -0300

    add given files

---
+++ montador.cpp
@@ -0,0 +1,44 @@
+// https://codeforces.com/blog/entry/125435
+#ifdef MAXWELL_LOCAL_DEBUG
+#include "debug/debug_template.cpp"
+#else
+#define debug(...)
+#define debugArr(arr, n)
+#endif
+
+#define dbg debug()
+
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+#define pb push_back
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using vi = vector<int>;
+
+using tii = tuple<int,int,int>;
+// auto [a,b,c] = ...
+// .insert({a,b,c})
+
+const int oo = (int)1e9 + 5; //INF to INT
+const ll OO = 0x3f3f3f3f3f3f3f3fLL; //INF to LL
+
+void solve() {
+    
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+}
