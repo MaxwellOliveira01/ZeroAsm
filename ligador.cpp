a21f85f8262751bba54707ddabb1db9dd5dce45a
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2024-12-23 20:13:38 -0300

    added an empty ligador.cpp

---
+++ ligador.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/blog/entry/125435
+#ifdef MAXWELL_LOCAL_DEBUG
+#include "debug/debug_template.cpp"
+#else
+#define debug(...)
+#define debugArr(arr, n)
+#endif
+
+#define dbg debug()
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    // ./ligador < prog1.obj prog2.obj > prog1.e
+
+}
