b1953ad16ff6f1093df57bae70d3c96f7216db45
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2024-12-23 20:13:13 -0300

    added a new sample with a lot of unnecessary space and tabs

---
+++ samples/2/in2.asm
@@ -0,0 +1,20 @@
+SECTION TEXT
+BEGIN : MOD1 ;comentario1
+MOD2    : EXTERN; comentario 2
+Public L2
+Public OLD_DATA
+    Public NEW_DATA
+ Public TMP_DATA
+
+;comentario 3
+
+INPUT OLD_DATA
+ LOAD OLD_DATA
+    jmp mod2; comentario 4
+                L2: STOP    ;comentario 5
+   END
+        SECTION DATA
+DOIS: CONST 2
+OLD_DATA: SPACE             ;;;;;;; comentario 6
+NEW_DATA: SPACE;        ;comentario7
+TMP_DATA: SPACE;comentario8;
