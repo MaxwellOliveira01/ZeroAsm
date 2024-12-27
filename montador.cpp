ca8b3a6d543fc84363f39cceeb4bb436d0b74af4
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2024-12-27 10:28:13 -0300

    Create and output pre process result to an file

--- montador.cpp
+++ montador.cpp
@@ -13,12 +13,33 @@
 
 using namespace std;
 
+// ./montador < input.asm > input.pre
+// ./montador < input.pre > input.obj
+
 int main(int argc, char *argv[]) {
 
-    // ./montador < input.asm > input.pre
-    // ./montador < input.pre > input.obj
+    auto [fileName, extension] = parseFileName(argv[1]);
 
-    auto preProcess = preProcessFile(argv[1]);
+    if(toLower(extension) == "asm") {
+
+        string outputName = fileName + ".pre"; 
+
+        // just create an empty file for now
+        ofstream empty(outputName);
+        empty.close();
+
+        auto preProcessedResult = preProcessFile(argv[1]);
+
+        ofstream res(outputName);
+        res << preProcessedResult.toString();
+        res.close();
+
+    } else if(toLower(extension) == "pre") {
+
+    } else {
+        showErrorAndExit("Invalid file extension, it must be .asm or .pre");
+    }
+
     return 0;
 
 }
