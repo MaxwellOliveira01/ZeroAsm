4f706c925c6675b9c54ab2614a9ad15a6fd3e30f
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2025-01-03 18:32:26 -0300

    Minor ref

--- code/assembler.cpp
+++ code/assembler.cpp
@@ -73,28 +73,26 @@ Program getProgram(PreProcessedData givenData, PreProcessedText givenText) {
             continue;
         }
 
-        if(symbolsTable.find(command->arg) == symbolsTable.end()) {
-            showErrorAndExit("'" + command->arg + "' is not defined");
-        } else {
-            // do we need to block the case of A + x, where A is CONST?
-            // do we need to block the case of A + 1, when A is just an label on text section?
-            // do we need to block the case of A + 10, when A is an label to SPACE 5?
-        }
+        vector<string> args = { command->arg };
 
         if(command->type == CommandType::Copy) {
             auto copyCommand = dynamic_cast<CopyCommand*>(command);
+            args.push_back(copyCommand->arg2);
+        }
 
-            if(symbolsTable.find(copyCommand->arg2) == symbolsTable.end()) {
-                showErrorAndExit("'" + copyCommand->arg2 + "' is not defined");
+        for(auto &arg : args) {
+            if(symbolsTable.find(arg) == symbolsTable.end()) {
+                showErrorAndExit("'" + arg + "' is not defined");
             } else {
                 // do we need to block the case of A + x, where A is CONST?
                 // do we need to block the case of A + 1, when A is just an label on text section?
                 // do we need to block the case of A + 10, when A is an label to SPACE 5?
             }
-
         }
 
     }
+
+    // if is not an module, it shouldnt have public or extern
 
     return program;
 }
